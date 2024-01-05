a26425ec27419ee8c3fe8862312ba77a160f7993
Dotrako <laagad94@gmail.com>
2024-01-05 22:55:08 +0100

    Add initial setup for email functionality using SMTP

--- Send_Email.py
+++ Send_Email.py
@@ -5,3 +5,9 @@ import smtplib # used to send email using SMTP (simple mail transfer protocol)
 from email.message import EmailMessage # importing EmailMessage class to use 'subject', 'from', 'to', 'cc' etc...
 from email.utils import formataddr
 from pathlib import Path
+
+port = "use your ports"
+Email_server = "test@outlook.com"
+
+# load the environment variables
+current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
