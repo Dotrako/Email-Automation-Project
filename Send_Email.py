b0043394656bf6e8f13ab3a3ec65cb5b9d9493d2
Dotrako <laagad94@gmail.com>
2024-01-05 23:05:49 +0100

    Add dotenv integration for environment variable management in email functionality setup

--- Send_Email.py
+++ Send_Email.py
@@ -5,9 +5,16 @@ import smtplib # used to send email using SMTP (simple mail transfer protocol)
 from email.message import EmailMessage # importing EmailMessage class to use 'subject', 'from', 'to', 'cc' etc...
 from email.utils import formataddr
 from pathlib import Path
+from dotenv import load_dotenv
 
 port = "use your ports"
 Email_server = "test@outlook.com"
 
 # load the environment variables
 current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
+envars = current_dir/".env"
+load_dotenv(envars)
+
+# Read environment variables
+sender_email = os.getenv("EMAIL")
+password_email = os.getenv("Password")
